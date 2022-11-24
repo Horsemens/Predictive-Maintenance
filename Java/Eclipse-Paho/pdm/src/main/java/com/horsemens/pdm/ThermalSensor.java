26b4b97e2a80d3459d58e48080b26fd21d5ccba7
Ayush Shende <ayushshende10@gmail.com>
2022-11-24 10:06:10 +0530

    testing branch

--- Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ThermalSensor.java
+++ Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ThermalSensor.java
@@ -4,7 +4,7 @@ public class ThermalSensor {
     public ThermalSensor(BrokerConfig brokerConfig, String topic){
         Sensor s = new Sensor(brokerConfig);
 
-        for (int i=100; i<200; i++){
+        for (int i=100; i<20000; i++){
             s.publish(Integer.toString(i), topic, 2);
         }
 
