0734db2ae01d941331cc93ca72b79deb9554e4ce
Pratik Patil <pratikap41@gmail.com>
2023-02-08 01:35:15 +0530

    updated CsvSourcePublisher

--- Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/CsvSourcePublisher.java
+++ Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/CsvSourcePublisher.java
@@ -43,7 +43,7 @@ public class CsvSourcePublisher {
 
                 //publishing
                 ecu.publish(jsonObject.toJSONString(), 2);
-                Thread.sleep(10000);
+                Thread.sleep(60000);
             }
 
         } catch (Exception e) {
