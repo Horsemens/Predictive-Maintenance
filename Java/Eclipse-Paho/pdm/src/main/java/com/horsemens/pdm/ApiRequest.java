89cbb5d4e92437d01b09cdd1ce499acb8af318ce
somesh <someshchinnawar222@gmail.com>
2023-04-16 20:14:06 +0530

    api request implemented

--- Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ApiRequest.java
+++ Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ApiRequest.java
@@ -1,21 +1,33 @@
 package com.horsemens.pdm;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.net.URI;
 
 public class ApiRequest {
-    
+
     public void sendData(String data){
+        System.out.println(data);
         String url = System.getenv("API_URL");
         if(url == null){
-            System.out.println("Using defautl url http://localhost:8000");
-            url = "http://localhost:8000";
+            System.out.println("Using defautl url http://localhost:8000/api/test");
+            url = "http://localhost:8000/api/insert/";
         }
         try{
+
             HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI(url))
-            .headers("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(data))
-            .build();
+                                            .uri(new URI(url))
+                                            .POST(HttpRequest.BodyPublishers.ofString(data))
+                                            .build();
+
+            HttpResponse<String> response = HttpClient
+                                            .newBuilder()
+                                            .build()
+                                            .send(request, BodyHandlers.ofString());
+
+            System.out.println(response);
+            
         }catch(Exception e){
             e.printStackTrace();
         }
