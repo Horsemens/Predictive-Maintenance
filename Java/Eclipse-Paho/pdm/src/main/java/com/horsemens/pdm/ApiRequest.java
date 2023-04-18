bf789b3dacff8a89aae3ee04879fed6a4568e97c
somesh <someshchinnawar222@gmail.com>
2023-04-18 22:47:44 +0530

    image ready

--- Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ApiRequest.java
+++ Java/Eclipse-Paho/pdm/src/main/java/com/horsemens/pdm/ApiRequest.java
@@ -6,7 +6,11 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.net.URI;
 
 public class ApiRequest {
-
+    // public static void main(String args[]){
+    //     String data = "{\"SensorId\":\"ecu1\",\"IgnitionTiming[Angle]\":\"248.0\",\"AFRDifference[AFR]\":\"13.0\",\"MAPSource[Pressure]\":\"399.0\",\"BaseFuel[Percentage]\":\"335.0\",\"CoolantTemp[Temperature]\":\"3242.0\",\"BatteryVoltage[BatteryVoltage]\":\"13075.0\",\"DateTime\":\"2023-04-16T11:52:05.210498293\",\"AirTemp[Temperature]\":\"3016.0\",\"RPM[EngineSpeed]\":\"1027.0\",\"LambdaSensor1[AFR]\":\"989.0\",\"BaseIgnition[AngleIgnSprt2K]\":\"320.0\"}";
+    //     new ApiRequest().sendData(data);
+    // }
+    
     public void sendData(String data){
         System.out.println(data);
         String url = System.getenv("API_URL");
